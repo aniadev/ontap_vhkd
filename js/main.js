f5ea83006c4bd5d8c9639445f5dd3e1f8822c398
phamhai-laptop <phamcong.hai.90@gmail.com>
2021-11-02 10:25:14 +0700

    add jquery, shuffle quizz

--- js/main.js
+++ js/main.js
@@ -5,9 +5,12 @@ var nextButton = document.getElementById("nextButton");
 var answerChecker = document.getElementById("answerChecker");
 var pointSpan = document.getElementById("pointSpan");
 
+let questions = quizz;
+shuffle(questions);
+
 nextButton.onclick = (e) => {
   level++;
-  if (level >= quizz.length) level = 0;
+  if (level >= questions.length) level = 0;
   a1.checked = a2.checked = a3.checked = a4.checked = false;
   submitButton.disabled = false;
   answerChecker.innerHTML = "";
@@ -29,11 +32,11 @@ submitButton.onclick = (e) => {
     submitButton.disabled = true;
     nextButton.disabled = false;
     // check correct
-    if (answer === quizz[level].s) {
+    if (answer === questions[level].s) {
       answerChecker.innerHTML = "CORRECT";
       points++;
     } else {
-      answerChecker.innerHTML = "INCORRECT, true answer: " + quizz[level].s;
+      answerChecker.innerHTML = "INCORRECT, true answer: " + questions[level].s;
     }
   } else {
     answerChecker.innerHTML = "No choice";
@@ -46,19 +49,23 @@ nextButton.disabled = true;
 renderLevel(level);
 //================================================================
 function renderQuestion(level) {
-  document.getElementById("q").innerHTML = quizz[level].q;
-  a1.labels[0].innerHTML = "A: " + quizz[level].a.A;
-  a2.labels[0].innerHTML = "B: " + quizz[level].a.B;
-  a3.labels[0].innerHTML = "C: " + quizz[level].a.C;
-  a4.labels[0].innerHTML = "D: " + quizz[level].a.D;
+  document.getElementById("q").innerHTML = questions[level].q;
+  a1.labels[0].innerHTML = "A: " + questions[level].a.A;
+  a2.labels[0].innerHTML = "B: " + questions[level].a.B;
+  a3.labels[0].innerHTML = "C: " + questions[level].a.C;
+  a4.labels[0].innerHTML = "D: " + questions[level].a.D;
 }
 //=============
 function renderLevel(level) {
   document.getElementById("levelSpan").innerHTML = `${level + 1} / ${
-    quizz.length
+    questions.length
   }`;
 }
 //=============
 function renderPoints() {
   pointSpan.innerHTML = `Correct: ${points} / ${level + 1}`;
+}
+//=============
+function shuffle(array) {
+  array.sort(() => Math.random() - 0.5);
 }
