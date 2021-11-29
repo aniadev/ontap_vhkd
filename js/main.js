147567b7a757790cd24cda81bb01e7e19a22c446
phamhai-laptop <phamcong.hai.90@gmail.com>
2021-11-29 20:27:42 +0700

    fix ui/ux

--- js/main.js
+++ js/main.js
@@ -3,45 +3,48 @@ var points = 0;
 var submitButton = document.getElementById("submitButton");
 var nextButton = document.getElementById("nextButton");
 var answerChecker = document.getElementById("answerChecker");
+var answerResult = document.getElementById("answerResult")
 var pointSpan = document.getElementById("pointSpan");
 
 let questions = quizz;
 shuffle(questions);
 
 nextButton.onclick = (e) => {
-  level++;
-  if (level >= questions.length) level = 0;
-  a1.checked = a2.checked = a3.checked = a4.checked = false;
-  submitButton.disabled = false;
-  answerChecker.innerHTML = "";
-  renderQuestion(level);
-  renderLevel(level);
-  nextButton.disabled = true;
+    level++;
+    if (level >= questions.length) level = 0;
+    a1.checked = a2.checked = a3.checked = a4.checked = false;
+    submitButton.disabled = false;
+    answerChecker.innerHTML = "";
+    answerResult.innerHTML = "";
+    renderQuestion(level);
+    renderLevel(level);
+    nextButton.disabled = true;
 };
 
 submitButton.onclick = (e) => {
-  // check answer
-  var answer;
-  if (a1.checked || a2.checked || a3.checked || a4.checked) {
-    answer =
-      (a1.checked && "A") ||
-      (a2.checked && "B") ||
-      (a3.checked && "C") ||
-      (a4.checked && "D");
-    // console.log(answer);
-    submitButton.disabled = true;
-    nextButton.disabled = false;
-    // check correct
-    if (answer === questions[level].s) {
-      answerChecker.innerHTML = "CORRECT";
-      points++;
+    // check answer
+    var answer;
+    if (a1.checked || a2.checked || a3.checked || a4.checked) {
+        answer =
+            (a1.checked && "A") ||
+            (a2.checked && "B") ||
+            (a3.checked && "C") ||
+            (a4.checked && "D");
+        // console.log(answer);
+        submitButton.disabled = true;
+        nextButton.disabled = false;
+        // check correct
+        if (answer === questions[level].s) {
+            answerChecker.innerHTML = "ĐÚNG";
+            points++;
+        } else {
+            answerChecker.innerHTML = "SAI";
+            answerResult.innerHTML = "Đáp án đúng: " + questions[level].s;
+        }
     } else {
-      answerChecker.innerHTML = "INCORRECT, true answer: " + questions[level].s;
+        answerChecker.innerHTML = "No choice";
     }
-  } else {
-    answerChecker.innerHTML = "No choice";
-  }
-  renderPoints();
+    renderPoints();
 };
 
 renderQuestion(level);
@@ -49,23 +52,23 @@ nextButton.disabled = true;
 renderLevel(level);
 //================================================================
 function renderQuestion(level) {
-  document.getElementById("q").innerHTML = questions[level].q;
-  a1.labels[0].innerHTML = "A: " + questions[level].a.A;
-  a2.labels[0].innerHTML = "B: " + questions[level].a.B;
-  a3.labels[0].innerHTML = "C: " + questions[level].a.C;
-  a4.labels[0].innerHTML = "D: " + questions[level].a.D;
+    document.getElementById("q").innerHTML = questions[level].q;
+    a1.labels[0].innerHTML = "A: " + questions[level].a.A;
+    a2.labels[0].innerHTML = "B: " + questions[level].a.B;
+    a3.labels[0].innerHTML = "C: " + questions[level].a.C;
+    a4.labels[0].innerHTML = "D: " + questions[level].a.D;
 }
 //=============
 function renderLevel(level) {
-  document.getElementById("levelSpan").innerHTML = `${level + 1} / ${
+    document.getElementById("levelSpan").innerHTML = `${level + 1} / ${
     questions.length
   }`;
 }
 //=============
 function renderPoints() {
-  pointSpan.innerHTML = `Correct: ${points} / ${level + 1}`;
+    pointSpan.innerHTML = `Kết quả: ${points} / ${level + 1}`;
 }
 //=============
 function shuffle(array) {
-  array.sort(() => Math.random() - 0.5);
+    array.sort(() => Math.random() - 0.5);
 }
